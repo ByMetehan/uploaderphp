00db1e180d41dd7f78a8d111f8aff53d6a2b233e
ByMetehan <63810726+ByMetehan@users.noreply.github.com>
2020-04-27 10:06:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Dosya Yükleyici </title>
+</head>
+<body>
+	<form action="upload.php" method="POST" enctype="multipart/form-data">
+
+<input type="file" name="file">
+
+<button type="submit" name="submit"> Yükle </button>
+
+
+
+	</form>
+
+</body>
+</html>
