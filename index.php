559d0b4b031e70110503b661dc71e5df6c3678d2
ByMetehan <63810726+ByMetehan@users.noreply.github.com>
2020-05-18 16:22:26 +0300

    Made index English

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title> Dosya Yükleyici </title>
+<title> File Uploader </title>
 </head>
 <body>
 	<form action="upload.php" method="POST" enctype="multipart/form-data">
 
 <input type="file" name="file">
 
-<button type="submit" name="submit"> Yükle </button>
+<button type="submit" name="submit"> Send </button>
 
 
 
